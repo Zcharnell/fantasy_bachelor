9f7a2d1a8213ef42360983e1fa792bca37cb16f3
zcharnell <zcharnell@gmail.com>
2018-05-14 00:46:29 -0700

    Update readme, remove extra files

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fantasy Bachelor Bracket Creator
+
+This is a project I put together in a couple of days back in January for the most recent season of Bachelor. By no means is it a great representation of React best practices or the use of Lambda, though it provides a good look at a rapid prototype.
+
+Uses React for the front end and Lambda + DynamoDB for the back end.
+
+## Directories
+
+** website **
+Contains the code for the website, including ESLint profile and script for deploying to S3.
+
+** lambda_functions **
+The code used in AWS Lambda functions. Post functions, for users and contestants, are triggered by an API call from the React site. Functions for calculating points are manually executed in Lambda to grab weekly data.
+
+** dynamodb **
+Extremely basic scripts for creating tables and loading data to DynamoDB. Check the Lambda functions for more detailed use of DynamoDB.
--- website/src/index.js
+++ website/src/index.js
@@ -4,22 +4,16 @@ import createLogger from 'redux-logger';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { browserHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from 'reducers';
 import middlewareApi from 'middleware/api';
-// import { loadColumnConfig } from 'actions/tableConfig'
-// import { loadAllRegions } from './actions/regions'
 import styles from 'index.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import routes from 'routes';
-import { Router } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-// import columnConfig from './json/dataColumns.json'
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 const loggerMiddleware = createLogger();
@@ -39,9 +33,6 @@ const configureStore = (preloadedState) => {
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
-
-// store.dispatch(loadAllRegions())
-// store.dispatch(loadColumnConfig(columnConfig))
 
 render(
   <Provider store={store}>
